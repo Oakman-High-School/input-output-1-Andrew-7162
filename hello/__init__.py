35fd732828fa8689deb69f5deb1ec5ce7e1751e0
Andrew-7162 <andrew.levrett7162@gmail.com>
2024-08-30 08:37:57 -0500

    Update __init__.py

--- hello/__init__.py
+++ hello/__init__.py
@@ -8,9 +8,13 @@
 #
 # ******************************************************************************
 
-"""'Hello, world!' module."""
+"""'Hello, world!' module.""" 
 
 from .hello import goodbye
 from .hello import goodbye_world
 from .hello import hello
 from .hello import hello_world
+print "Hello!"
+print "Goodbye!"
+print "Hello, world!"
+print "Goodbye, world!'
